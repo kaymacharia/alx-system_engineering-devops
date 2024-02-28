b6759af78ef7428a7508aa91baaaaa81ddd9bab1
kaymacharia <kaymachariaa@gmail.com>
2024-02-28 13:01:57 -0800

    1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,15 @@
-# Changes the SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+# Puppet manifest to configure SSH client
+
+file_line { 'Turn off passwd auth':
+   ensure  => present,
+   path    => '/etc/ssh/ssh_config',
+   line    => 'PasswordAuthentication no',
+   replace => true,
+}
+
+file_line { 'Declare identity file':
+   ensure  => present,
+   path    => '/etc/ssh/ssh_config',
+   line    => 'IdentityFile ~/.ssh/school',
+   replace => true,
 }
